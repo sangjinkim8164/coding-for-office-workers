3884c27694ecead95bdf0d31aabd8f75748525b3
sangjinkim8164 <sangjinkim8164@gmail.com>
2017-01-15 18:03:25 +0900

    Add recommanding-food-store.py

---
+++ recommanding food store.py
@@ -0,0 +1,13 @@
+# 식당 추천
+a = input("한식, 중식, 일식 중 한 가지를 고르세요")
+b = ["김밥천국", "계절밥상", "스쿨푸드"]
+c = ["차이나팩토리", "상하이짬뽕", "메이징에이"]
+d = ["용우동", "명동참치", "사조참치"]
+
+if a is "한식"
+    print("'{}' 추천합니다.format(ramdom.choice(b)))
+
+elif a is "중식"
+    print("'{}' 추천합니다.format(ramdom.choice(b)))
+    else
+        print("'{}' 추천합니다.format(ramdom.choice(c)))
